d9f0066290e57f99ae2d0a95ef7e90867d1b1e0d
Catherine (Katie) Gosmeyer <cgosmeyer@stsci.edu>
2016-12-21 17:34:01 -0500

    Working writecol and started writing nosetests.

--- muttontools/col_readwrite.py
+++ muttontools/col_readwrite.py
@@ -19,6 +19,7 @@ import os
 import re
 import sys
 
+
 #-----------------------------------------------------------------------------#
 
 def readcol(filename, headerstart=0, datastart=1, comment=' ', 
@@ -42,93 +43,99 @@ def readcol(filename, headerstart=0, datastart=1, comment=' ',
 
     Returns
     -------
+    header : list of strings
+        List of the column names if they exist in row specified by 
+        'headerstart'. If first column name contains a '#' it will 
+        be removed. If no column names exist, then returns a list 
+        of string numbers, starting at '0'. 
+    cols : list of lists
+        List of the columns, each their own list. If no columns found,
+        returns an empty list. 
 
     future improvements:
     - option to return a dictionary??
-    - rewrite as class?
-    - how make fewer loops?? 
+
     """
-    # Make sure file exists.
-    #try:    
-    #    f = open(filename, 'r')
-    #except IOError:
-    #    print("File {} does not exist.".format(filename))
-    #    return [], []
 
-    #print("reading {}".format(filename))
-    cols = []
-    i = 0
-     
-    # Make sure that \t, \n, etc are still being split 
-    if deliminator != "\s+":
-        deliminator = '[' + deliminator + '\s\+]'
-
+    # Try and Except to exit gracefully and return empty lists
+    # if the file does not exist.
+    # (but if end up not returning empty lists, could try to 
+    # re-write the with loop into the try-with)
     try: 
+        # Open using with so that if error occurs the file will
+        # be closed properly.
         with open(filename, 'r') as f:
+            print("reading {}".format(filename))
+
+            # Initialize counts and lists.
+            cols = []
+            row_count = 0
+
+            # Make sure that \t, \n, etc will still be split out.
+            if deliminator != "\s+":
+                deliminator = '[' + deliminator + '\s\+]'
+
             # not too happy with the with then for structure
             # but better to use with so that file always is closed.
             # how to return custom output if exception occurs? 
             for line in f:
 
-                print("reading {}".format(filename))
-
-                print(line)
+                # Change line into list of entires, split by the 
+                # deliminator.
                 linestrip = re.split(deliminator, line)
+
+                # Remove all empty strings.
                 while '' in linestrip:
                     linestrip.remove('')
-                
-                print(i, headerstart, datastart)
-                print(linestrip)
 
                 # First test that line is not empty.
-                if linestrip != []:
+                # Second test whether the first line is in fact
+                # a custom comment.
+                # If the user chooses to have a comment above the header,
+                # and that header is denoted by the same comment marker,
+                # it is on the user to choose correctly the 'headerstart'.
+                if linestrip != [] and linestrip[0][0] != comment:
 
-                    # Second test whether the first line is in fact
-                    # a custom comment.
-                    # If the user chooses to have a comment above the header,
-                    # and that header is denoted by the same comment marker,
-                    # it is on the user to choose correctly the 'headerstart'.
-
-                    if linestrip[0] != comment:
-
-                        # If there is a header, retrieve the column names.
-                        if i == headerstart and headerstart != datastart:
-                                # need to fix so that '#' is headercomment
-                                # Figure out how many columns and their names.
-                            if linestrip[0] == '#':
-                                ncols = len(linestrip)-1
-                                linestrip.remove('#')
-                            else:
-                                ncols = len(linestrip)
-                            print('ncols: {}'.format(ncols))
-                            header = linestrip
-                            cols = [[] for j in range(ncols)]
-
-                            # Remove comment if first character.
-                            if header[0][0] == '#':
-                                header[0] = header[0][1:]
-                            print("header: {}".format(header))
-
-                        # If there is no header, just name the columns
-                        # by number.
-                        elif i == datastart and datastart == headerstart:
+                    # If there is a header, retrieve the column names.
+                    if row_count == headerstart and headerstart != datastart:
+                        # Figure out how many columns and their names.
+                        if linestrip[0] == '#':
+                            ncols = len(linestrip)-1
+                            linestrip.remove('#')
+                        else:
                             ncols = len(linestrip)
-                            header = list(np.arange(ncols))
-                            cols = [[] for j in range(ncols)]
+                        print('ncols: {}'.format(ncols))
+                        header = linestrip
+                        # Initilize empty list for each column.
+                        cols = [[] for row in range(ncols)]
 
-                        elif i >= datastart and linestrip != []:
-                            for item, j in zip(linestrip, range(ncols)):
-                                cols[j].append(item)
-                        i+=1
+                        # Remove comment if first character.
+                        if header[0][0] == '#':
+                            header[0] = header[0][1:]
 
-        #f.close()
+                    # If there is no header, just name the columns
+                    # by number.
+                    elif row_count == datastart and datastart == headerstart:
+                        ncols = len(linestrip)
+                        header = list(map(str, np.arange(ncols)))
+                        # Initilize empty list for each column.
+                        cols = [[] for row in range(ncols)]
+
+                    # For all rows not in header, not empty, and not 
+                    # a comment, append to column list.
+                    # This is an if so that it can execute should
+                    # headerstart = datastart = 0.
+                    if row_count >= datastart and linestrip != []:
+                        for item, row in zip(linestrip, range(ncols)):
+                            cols[row].append(item)
+                    row_count+=1
 
         # Check whether the file was empty or no valid columns read.
         if cols == []:
             print("No valid columns found for file {}.".format(filename))
             return [], []
         else:
-            return cols, header
+            return header, cols
 
     except IOError:
         print("File {} does not exist.".format(filename))
@@ -146,51 +153,79 @@ def writecol(filename, data, header=[], deliminator=' ', headerstarter='# ',
     Parameters
     ----------
     filename : str
-
-    data :
-
-    header : 
-
+        Name of the file to write to.
+    data : list of lists
+        Can contain any type. The lists will become the columns and must 
+        all be the same length.
+    header : list
+        Optional. The names of the columns.
     deliminator : str
-
+        Optional. How to divide the columns. A single space by default.
     headerstarter : str
-
+        Optional. String with which to start a header. '#' by default.
     writer : str
-
+        Optional. The file writing preference. 'a+' by default.
     overwrite : {True, False}
-
+        Optional. Whether to overwrite (if writer = 'w') or append (if
+        writer = 'a') if the file already exists. False by default.
+        Meant as a dummy-proof in case you didn't enter the 'writer' you
+        intended.
 
     Returns
     -------
 
-    - have a check for overwriting. give user a warning?
     """
 
-    f = open(filename, writer)
+    # Check for overwriting
+    if os.path.isfile(filename) and not overwrite:
+        print("File '{}' already exists and overwrite is set to False. Halting..."\
+            .format(filename))
+        return
+
+    elif os.path.isfile(filename) and overwrite:
+        print("File '{}' already exists but overwriting/appending with '{}'..."\
+            .format(filename, writer))
+
+    else:
+        print("File '{}' is being created...".format(filename))
+
+    # Record number of columns.
     ncols = len(data)
-    nrows = len(data[0])
 
-    if header != []:
-        headerline = headerstarter
-        for j in range(ncols):
-            if j == ncols-1:
-                headerline += header[j] + '\n'
-            else:
-                headerline += header[j] + deliminator 
-        f.write(headerline)
+    # Check that all columns are same length.
+    if not all(len(col) == len(data[0]) for col in data):
+        print("Error: columns not all same length.") 
+        return 
+    else:
+        # Record number of rows.
+        nrows = len(data[0])
+
+    # Check that a header, if given, matches number of columns.
+    if header != [] and len(header) != ncols:
+        print("Error: length of header, {}, does not match number of columns, {}."\
+            .format(len(header), ncols)) 
+        return
+
+    with open(filename, writer) as f:
+
+        if header != []:
+            headerline = headerstarter
+            for j in range(ncols):
+                if j == ncols-1:
+                    headerline += header[j] + '\n'
+                else:
+                    headerline += header[j] + deliminator 
+            f.write(headerline)
 
 
-    for i in range(nrows):
-        print(i)
-        line = ''
-        for j in range(ncols):
-            if j == ncols-1:
-                line += data[j][i] + '\n'
-            else:
-                line += data[j][i] + deliminator
-        
-        f.write(line)
-
-    f.close()
+        for i in range(nrows):
+            line = ''
+            for j in range(ncols):
+                if j == ncols-1:
+                    line += data[j][i] + '\n'
+                else:
+                    line += data[j][i] + deliminator
+            
+            f.write(line)
 
 
--- tests/test_col_readwrite.py
+++ tests/test_col_readwrite.py
@@ -4,52 +4,128 @@ Nose tests for file reader and writer.
 
 Use:
 
-    >>> 
+    >>> nosetests
 
 Test directory includes the test files:
 
-	*
-	*
+    *
+    *
 """
+
+import os
 
 from muttontools.col_readwrite import readcol, writecol
 from nose.tools import *
+from IPython.utils.capture import capture_output
+
 
 # Tests for readcol
 
-def test_nominal_file():
-	assert readcol(filename='nominal.txt') == ([['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], ['x', 'y'])
+def test_reads_nominal_file():
+    assert readcol(filename='nominal.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_skips_custom_comment_lines():
-	assert readcol(filename='comments_custom.txt', comment='%') == ([], [])	
-	assert readcol(filename='comments_custom2.txt', comment='%') == ([['1', '2', '3'], ['.1', '.2', '.3']], ['x', 'y'])
+    assert readcol(filename='comments_custom.txt', comment='%') == ([], []) 
+    assert readcol(filename='comments_custom2.txt', comment='%') == (['x', 'y'], [['1', '2', '3'], ['.1', '.2', '.3']])
 
 def test_exits_gracefully_if_no_file():
-	assert readcol(filename='noexistence.txt') == ([], [])
+    assert readcol(filename='noexistence.txt') == ([], [])
 
 def test_finds_header_and_data_if_not_default():
-    assert readcol(filename='nondefault_start.txt', headerstart=4, datastart=6) == ([['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], ['x', 'y'])
+    assert readcol(filename='nondefault_start.txt', headerstart=4, datastart=6) == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_reads_empty_file_gracefully():
     assert readcol(filename='empty.txt') == ([], [])
 
 def test_can_mix_space_and_tab():
-    assert readcol(filename='mixed_space_tab.txt') == ([['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], ['x', 'y'])
+    assert readcol(filename='mixed_space_tab.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_can_read_custom_deliminator():
-   assert readcol(filename='custom_deliminator.txt', deliminator="|") == ([['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], ['x', 'y'])
+   assert readcol(filename='custom_deliminator.txt', deliminator="|") == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+
+def test_can_read_single_column_file():
+    assert readcol(filename='single_col.txt') == (['x'], [['1', '2', '3', '4']])    
+
+def test_can_read_no_header():
+    assert readcol(filename='noheader.txt', headerstart=0, datastart=0) == (['0', '1'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+
 
 # Tests for writecol
-#def test_exits_gracefully_if_cols_headerlen_mismatched():
 
-#def test_doesnot_overwrite_file_if_False():
+def test_writes_nominal_file():
+    """ If this test does not work, all remaining tests will fail 
+    because rely on generation of a nominal file. 
+    """
+    filename='nominal_write.txt'
+    writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y'])
+    assert readcol(filename=filename) == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+    os.remove(filename)
 
-#def test_does_overwrite_file_if_True():
+def test_doesnt_modify_file_if_overwrite_False():
+    filename='test_overwrite_false.txt'
+    writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y'])
+    with capture_output() as c:
+        writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y'], overwrite=False)
+    c()
+    out = c.stdout
+    assert out.split('\n')[0] == "File '{}' already exists and overwrite is set to False. Halting...".format(filename)
+    os.remove(filename)
 
-#def cleanup():
-#	""" 
-#    Removes all generated test files.
-#	"""
+def test_does_append_file_if_overwrite_True():
+    """ writer = 'a+'
+    """
+    filename='test_append_overwrite_true.txt'
+    writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y']) 
+    with capture_output() as c:
+        writecol(filename=filename, data=[['5', '6'], ['.5', '.6']], writer='a+', overwrite=True)
+    c()
+    out = c.stdout
+    assert out.split('\n')[0] == "File '{}' already exists but overwriting/appending with '{}'..."\
+        .format(filename, 'a+')
+    assert readcol(filename=filename) == (['x', 'y'], [['1', '2', '3', '4', '5', '6'], ['.1', '.2', '.3', '.4', '.5', '.6']])
+    os.remove(filename)
+
+def test_does_overwrite_file_if_overwrite_True():
+    """ writer = 'w'
+    """
+    filename='test_writer_overwrite_true.txt'
+    writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y']) 
+    with capture_output() as c:
+        writecol(filename=filename, data=[['5', '6'], ['.5', '.6']], header=['x', 'y'], writer='w', overwrite=True)
+    c()
+    out = c.stdout
+    assert out.split('\n')[0] == "File '{}' already exists but overwriting/appending with '{}'..."\
+        .format(filename, 'w')
+    assert readcol(filename=filename) == (['x', 'y'], [['5', '6'], ['.5', '.6']])
+    os.remove(filename)
+
+def test_exits_gracefully_if_columns_not_same_len():
+    filename='test_write_columns_not_same_len.txt'
+
+    with capture_output() as c:
+        writecol(filename=filename, data=[['1', '2', '3'], ['.1', '.2', '.3', '.4']], header=['x', 'y', 'z']) 
+    c()
+    out = c.stdout
+    assert out.split('\n')[1] == "Error: columns not all same length."
+
+def test_exits_gracefully_if_ncols_headerlen_mismatched():
+    filename='test_write_mismatched_headerlen.txt'
+    # Header length greater than number of columns
+    with capture_output() as c:
+        writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x', 'y', 'z']) 
+    c()
+    out = c.stdout
+    assert out.split('\n')[1] == "Error: length of header, {}, does not match number of columns, {}."\
+        .format(3, 2)
+
+    # Header length less than number of columns
+    with capture_output() as c:
+        writecol(filename=filename, data=[['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']], header=['x']) 
+    c()
+    out = c.stdout
+    assert out.split('\n')[1] == "Error: length of header, {}, does not match number of columns, {}."\
+        .format(1, 2)
 
 
-#def test_appends_to_file():
+
+
