66c011ccd1c0ca2b4f7af619324a92b7fb700d41
Catherine (Katie) Gosmeyer <cgosmeyer@stsci.edu>
2016-12-21 18:38:18 -0500

    Fixing accidental move.

--- tests/test_colio.py
+++ tests/test_colio.py
@@ -14,40 +14,39 @@ Test directory includes the test files:
 
 import os
 
-from muttontools.col_readwrite import readcol, writecol
+from muttontools.colio import readcol, writecol
 from nose.tools import *
 from IPython.utils.capture import capture_output
-
 
 # Tests for readcol
 
 def test_reads_nominal_file():
-    assert readcol(filename='nominal.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+    assert readcol(filename='test_files/nominal.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_skips_custom_comment_lines():
-    assert readcol(filename='comments_custom.txt', comment='%') == ([], []) 
-    assert readcol(filename='comments_custom2.txt', comment='%') == (['x', 'y'], [['1', '2', '3'], ['.1', '.2', '.3']])
+    assert readcol(filename='test_files/comments_custom.txt', comment='%') == ([], []) 
+    assert readcol(filename='test_files/comments_custom2.txt', comment='%') == (['x', 'y'], [['1', '2', '3'], ['.1', '.2', '.3']])
 
 def test_exits_gracefully_if_no_file():
-    assert readcol(filename='noexistence.txt') == ([], [])
+    assert readcol(filename='test_files/noexistence.txt') == ([], [])
 
 def test_finds_header_and_data_if_not_default():
-    assert readcol(filename='nondefault_start.txt', headerstart=4, datastart=6) == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+    assert readcol(filename='test_files/nondefault_start.txt', headerstart=4, datastart=6) == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_reads_empty_file_gracefully():
-    assert readcol(filename='empty.txt') == ([], [])
+    assert readcol(filename='test_files/empty.txt') == ([], [])
 
 def test_can_mix_space_and_tab():
-    assert readcol(filename='mixed_space_tab.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+    assert readcol(filename='test_files/mixed_space_tab.txt') == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_can_read_custom_deliminator():
-   assert readcol(filename='custom_deliminator.txt', deliminator="|") == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+   assert readcol(filename='test_files/custom_deliminator.txt', deliminator="|") == (['x', 'y'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 def test_can_read_single_column_file():
-    assert readcol(filename='single_col.txt') == (['x'], [['1', '2', '3', '4']])    
+    assert readcol(filename='test_files/single_col.txt') == (['x'], [['1', '2', '3', '4']])    
 
 def test_can_read_no_header():
-    assert readcol(filename='noheader.txt', headerstart=0, datastart=0) == (['0', '1'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
+    assert readcol(filename='test_files/noheader.txt', headerstart=0, datastart=0) == (['0', '1'], [['1', '2', '3', '4'], ['.1', '.2', '.3', '.4']])
 
 
 # Tests for writecol
